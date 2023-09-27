700bc292051de8ac2a749931b8227f2a30d40aaf
SushantKhanal <dreamerking114@gmail.com>
2023-09-27 14:47:31 -0500

    test case added: testEquality

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,12 +1,19 @@
 package guru.springframework;
 
 public class Dollar {
-    int amount;
+
+    private int amount;
     public Dollar(int amount) {
         this.amount = amount;
     }
 
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object dollarObj) {
+        Dollar dollar = (Dollar) dollarObj;
+        return amount == dollar.amount;
     }
 }
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -1,14 +1,22 @@
 package guru.springframework;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10, product.amount);
+        assertEquals(new Dollar(10), product);
         product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(15), product);
+    }
+
+    @Test
+    void testEquality() {
+        Dollar dollar1 = new Dollar(5);
+        assertEquals(dollar1, new Dollar(5));
+        assertNotEquals(dollar1, new Dollar(10));
     }
 }
