185fdd6c01839365cb14f8bb31f8b34fde0964a8
SushantKhanal <dreamerking114@gmail.com>
2023-10-10 22:02:43 -0700

    test multiplication dollar and franc consolidated

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -5,10 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
     @Test
-    void testMultiplicationDollar() {
-        Money five = Money.dollar(5);
-        assertEquals(Money.dollar(10), five.times(2));
-        assertEquals(Money.dollar(15), five.times(3));
+    void testMultiplication() {
+        Money fiveDollar = Money.dollar(5);
+        assertEquals(Money.dollar(10), fiveDollar.times(2));
+        assertEquals(Money.dollar(15), fiveDollar.times(3));
+
+        Money fiveFranc = Money.franc(5);
+        assertEquals(Money.franc(10), fiveFranc.times(2));
     }
 
     @Test
@@ -17,13 +20,6 @@ public class MoneyTest {
         assertEquals(dollar1, Money.dollar(5));
         assertNotEquals(dollar1, Money.dollar(10));
         assertNotEquals(Money.dollar(5), Money.franc(5));
-    }
-
-    @Test
-    void testMultiplicationFranc() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
     }
 
     @Test
