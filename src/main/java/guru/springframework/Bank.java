d135d6f3dcf902964f256116575b6293ae6c5fa3
SushantKhanal <dreamerking114@gmail.com>
2023-10-11 17:09:30 -0700

    rate implementation and test cases

--- src/main/java/guru/springframework/Bank.java
+++ src/main/java/guru/springframework/Bank.java
@@ -1,7 +1,21 @@
 package guru.springframework;
 
+import java.util.HashMap;
+
 public class Bank {
+    private HashMap<Pair, Integer> rateMap = new HashMap<>();
     Money reduce(Expression source, String toCurrency) {
-        return source.reduce(toCurrency);
+        return source.reduce(this, toCurrency);
+    }
+    public int rate(String from, String to) {
+        Integer rate = rateMap.get(new Pair(from, to));
+        if(from.equals(to)) {
+            return 1;
+        }
+        return rate;
+    }
+
+    public void addRate(String from, String to, int rate) {
+        rateMap.put(new Pair(from, to), rate);
     }
 }
